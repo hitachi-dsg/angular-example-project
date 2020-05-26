f89641434b6337b3ddee487cf3c30cb16ddb5c53
Magdalena Warzecha <magdalena@Magdalenas-MacBook-Pro.local>
2020-05-26 20:00:56 +0100

    first commit

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit, NgModule } from '@angular/core';
+import { Hero } from "../hero";
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
+
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+
+  heroes: Hero[];
+
+  //Reserve the constructor for simple initialization such as wiring constructor parameters to properties.
+  constructor(private heroService: HeroService, private messageService: MessageService) {}
+
+  //define a method that will pull Heroes from the service and assign the value to the <heroes> property
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
+  }
+
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.addHero({ name } as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      });
+  }
+
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero).subscribe();
+  }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+}
